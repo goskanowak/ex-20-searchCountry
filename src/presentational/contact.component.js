cc42bfe6b6d08080f423ff2c74d49d3683de2a7d
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-10 12:58:57 +0200

    Add presentational components

---
+++ src/presentational/contact.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>    
+    <h1>To contact us:</h1>
+    <p>Street: Route 66</p>
+    <p>Phone: +48 859 586 258</p>
+  </div>
+);
+
+export default Contact;
