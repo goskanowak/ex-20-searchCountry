b90cdf1fce243bad85e8d54f0087abac32f363ef
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-11 19:00:25 +0200

    Add bootstrap@3

--- src/presentational/flag-list.component.js
+++ src/presentational/flag-list.component.js
@@ -13,7 +13,7 @@ const CountryFlagList = (props) => (
             <CountryFlag country={country} />
           </Link>
           <button 
-            onClick={props.deleteCountry.bind(null, country.id)}>DELETE
+            onClick={props.deleteCountry.bind(null, country.id)}> DELETE
           </button>
         </div>
       )
